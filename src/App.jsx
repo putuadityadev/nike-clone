079bad410b061544213167227b7a78258e30ec4a
aditya <adityamph1@gmail.com>
2024-10-31 23:30:57 +0800

    "Updated index.html and App.jsx with new favicon, title, and component imports/usage"

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
+import { CustomerReviews, Footer, Hero, PopularProduct, Services, SpecialOffers, Subscribe, SuperQuality } from "./Sections";
+import Nav  from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero/>
     </section>
     <section className="padding">
-      PopularProduct
+      <PopularProduct/>
     </section>
     <section className="padding">
-      Super Quality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      Special Offers
+      <SpecialOffers/>
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer/>
     </section>
   </main>
 );
