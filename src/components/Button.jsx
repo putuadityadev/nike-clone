7e561d9a6a628d7341a80b2ceefe74ded034bd55
aditya <adityamph1@gmail.com>
2024-11-16 15:45:24 +0800

    "Updated Services and SuperQuality sections with new components and layouts, and created a Button component"

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,11 @@ const Button = ({label, iconURL}) => {
         <button className='flex justify-center items-center gap-2 font-montserrat px-7 py-4 border leading-none bg-coral-red rounded-full text-white text-lg'>
             {label}
 
-            <img 
+            {iconURL && <img 
                 src={iconURL}
                 alt='arrow right icon'
                 className='ml-2 rounded-full w-5 h-5'
-            />
+            />}
         </button>
     )
 }
