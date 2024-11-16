0c34bbbd7c81aa7439218bd9e3745c3ee0445c51
aditya <adityamph1@gmail.com>
2024-11-16 14:30:51 +0800

    first commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
-
+import { navLinks } from '../constants/index.js';
 const Nav = () => {
     return (
         <header className='padding-x py-8 absolute z-10 w-full'>
@@ -15,8 +15,22 @@ const Nav = () => {
                     />
                 </a>
                 <ul className='flex-1 flex justify-center gap-16 max-lg:hidden'>
-                    
+                    {navLinks.map((item) => (
+                        <li key={item.label}>
+                            <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
+                <div className='lg:hidden'>
+                    <img
+                        src={hamburger}
+                        alt="hamburger"
+                        width={25}
+                        height={25}
+                    />
+                </div>
             </nav>
         </header>
     )
