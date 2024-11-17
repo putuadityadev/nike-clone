ebc7759c90919a1cff8d6630cb5b1226ad0b9595
aditya <adityamph1@gmail.com>
2024-11-17 12:38:13 +0800

    Update configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nike-clone/",
+  base: '/nike-clone/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
-
-
