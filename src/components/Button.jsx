d94de348662d9b4239fd2edefcf97bd8bfa19cd1
aditya <adityamph1@gmail.com>
2024-11-17 11:37:01 +0800

    "Updated CustomerReviews, Footer, SpecialOffers, Subscribe, and Button components with new JSX code"

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor, fullWidth}) => {
     return (
-        <button className='flex justify-center items-center gap-2 font-montserrat px-7 py-4 border leading-none bg-coral-red rounded-full text-white text-lg'>
+        <button className={`flex justify-center items-center gap-2 font-montserrat px-7 py-4 border leading-none
+            ${
+                backgroundColor
+                ? `${backgroundColor} ${textColor} ${borderColor}`
+                : 'bg-coral-red text-white text-lg'
+            } rounded-full ${fullWidth && 'w-full'}`}>
+        
             {label}
 
             {iconURL && <img 
